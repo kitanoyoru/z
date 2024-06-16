ba922a27e737ba32ebf17c3367488489bf19cca9
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2024-06-16 15:23:01 +0300

    chore: renamed packages

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "z",
-        .root_source_file = b.path("src/lib.zig"),
+        .root_source_file = b.path("lib/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -14,7 +14,7 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/lib.zig"),
+        .root_source_file = b.path("lib/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
