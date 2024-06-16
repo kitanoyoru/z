8cc9c3d7d7b685c38d67e8bd88cd5a827957b1d0
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2024-06-16 14:33:46 +0300

    feat(string): chunkString + tests

--- src/string.zig
+++ src/string.zig
@@ -5,7 +5,7 @@ pub fn randomString(allocator: std.mem.Allocator, size: usize, charset: []const 
         return error.InvalidSize;
     }
     if (charset.len == 0) {
-        return error.InvalidCharset;
+        return error.EmptyString;
     }
 
     const buffer = try allocator.alloc(u8, size);
@@ -26,7 +26,29 @@ pub fn randomString(allocator: std.mem.Allocator, size: usize, charset: []const 
     return buffer;
 }
 
-test "should generate random string with specified charset" {
+pub fn chunkString(allocator: std.mem.Allocator, s: []const u8, size: usize) ![]const []const u8 {
+    if (size == 0) {
+        return error.InvalidSize;
+    }
+    if (s.len == 0) {
+        return error.EmptyString;
+    }
+
+    const num_chunks = (s.len + size - 1) / size;
+    var chunks = try allocator.alloc([]const u8, num_chunks);
+
+    var chunk_idx: usize = 0;
+    while (chunk_idx < num_chunks) {
+        const start_idx = chunk_idx * size;
+        const end_idx = @min(start_idx + size, s.len);
+        chunks[chunk_idx] = s[start_idx..end_idx];
+        chunk_idx += 1;
+    }
+
+    return chunks;
+}
+
+test "randomString: should generate without errors" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
@@ -45,4 +67,50 @@ test "should generate random string with specified charset" {
         std.debug.print("Error: wrong generated size\n", .{});
         return;
     };
+}
+
+test "chunkString: should return without errors" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const input_str = "Hello, World!";
+    const chunk_size: usize = 5;
+
+    const chunks = try chunkString(allocator, input_str, chunk_size);
+    defer allocator.free(chunks);
+
+    try std.testing.expect(chunks.len == 3);
+    try std.testing.expectEqualSlices(u8, chunks[0], "Hello");
+    try std.testing.expectEqualSlices(u8, chunks[1], ", Wor");
+    try std.testing.expectEqualSlices(u8, chunks[2], "ld!");
+}
+
+test "chunkString: should return error.EmptyString" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const input_str = "";
+    const chunk_size: usize = 5;
+
+    const result = chunkString(allocator, input_str, chunk_size);
+
+    return std.testing.expectError(error.EmptyString, result);
+}
+
+test "chunkString: should return error.InvalidSize" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const input_str = "Hello, World!";
+    const chunk_size: usize = 0;
+
+    const result = chunkString(allocator, input_str, chunk_size);
+
+    return std.testing.expectError(error.InvalidSize, result);
 }
