835779e3f108b9fe21aa561fd7c78d45cba125df
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2024-06-16 15:46:44 +0300

    fixed: updated build.zig test stage

--- build.zig
+++ build.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+const test_targets = [_]std.Target.Query{
+    .{}, // native
+};
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -13,14 +16,21 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("lib/lib.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    for (test_targets) |test_target| {
+        const lib_string_unit_tests = b.addTest(.{
+            .root_source_file = b.path("lib/string.zig"),
+            .target = b.resolveTargetQuery(test_target),
+        });
+        const lib_find_unit_tests = b.addTest(.{
+            .root_source_file = b.path("lib/find.zig"),
+            .target = b.resolveTargetQuery(test_target),
+        });
+
+        const run_string_lib_unit_tests = b.addRunArtifact(lib_string_unit_tests);
+        const run_find_lib_unit_tests = b.addRunArtifact(lib_find_unit_tests);
+
+        test_step.dependOn(&run_string_lib_unit_tests.step);
+        test_step.dependOn(&run_find_lib_unit_tests.step);
+    }
 }
